9ccb5ab42ac07948d161734b2f30db3341038a16
Saint khen <72024308+emmogrin@users.noreply.github.com>
2025-05-01 17:02:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,30 +6,30 @@ pkg update -y && pkg upgrade -y
 echo "[2/4] Installing QEMU and dependencies..."
 pkg install -y qemu-system-x86_64 wget curl unzip proot pulseaudio
 
-echo "[3/4] Downloading Debian x86_64 image..."
-mkdir -p qemu-debian && cd qemu-debian
+echo "[3/4] Downloading Debian x86_64 image (minimal)..."
+mkdir -p ~/qemu-debian && cd ~/qemu-debian
 
-# Download rootfs
-wget https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-minirootfs-3.18.4-x86_64.tar.gz -O rootfs.tar.gz
+# Download prebuilt Debian disk image
+wget https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-extended-3.18.4-x86_64.iso -O debian.iso
 
-echo "[4/4] Setting up QEMU boot..."
-# Download a minimal Linux kernel
-wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-4.4.34-jessie -O vmlinuz
+# Download SeaBIOS (to help QEMU boot ISO)
+wget https://github.com/bmaron/qemu-seabios-android/releases/download/v1.0/bios.bin -O bios.bin
 
-# Create QEMU launch script
+echo "[4/4] Creating launch script..."
 cat > start.sh << 'EOF'
 #!/bin/bash
 qemu-system-x86_64 \
+  -bios bios.bin \
   -m 2048 \
-  -kernel vmlinuz \
-  -initrd initrd.img \
-  -hda rootfs.img \
-  -append "root=/dev/sda console=ttyS0" \
+  -cdrom debian.iso \
+  -boot d \
   -nographic \
-  -net nic -net user
+  -net nic -net user \
+  -vga std
 EOF
 
 chmod +x start.sh
 
-echo "Done! To start Debian x86_64, run:"
-echo "cd ~/qemu-x86-emulator-setup/qemu-debian && ./start.sh"
+echo
+echo "✅ Done! To boot Debian ISO:"
+echo "cd ~/qemu-debian && ./start.sh"
